1918d029e1073cf9bd4b9456889dd65a3c6bc927
AmjadAbuSara <amjaddiab234@gmail.com>
2025-05-30 01:51:52 +0300

    https://tn-horrible-scsi-ira.trycloudflare.com/

--- src/hooks/use-dashboard-data.js
+++ src/hooks/use-dashboard-data.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 // 🌐 Set your backend base URL
-const API_URL = "https://tn-horrible-scsi-ira.trycloudflare.com//dashboard-data/";
+const API_URL = "https://tn-horrible-scsi-ira.trycloudflare.com/dashboard-data/";
 // Example: const API_URL = "https://anything.trycloudflare.com/dashboard-data/";
 
 export const useDashboardData = () => {
